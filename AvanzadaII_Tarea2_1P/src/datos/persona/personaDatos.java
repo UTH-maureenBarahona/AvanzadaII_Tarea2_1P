b10d8b7c1d5d90b77afcda05e30b363ebca39735
Maureen Barahona <maureenbarahona@gmail.com>
2021-02-03 17:08:46 -0600

    cambio libreria

--- AvanzadaII_Tarea2_1P/src/datos/persona/personaDatos.java
+++ AvanzadaII_Tarea2_1P/src/datos/persona/personaDatos.java
@@ -25,12 +25,13 @@ public class personaDatos {
 
     public static int secuenciaCodPersona() throws SQLException {
         int cod = 0;
+
         try {
 
-            try (Connection cn = (Connection) conexion.ObtenerConexion()) {
+            try ( Connection cn = (Connection) conexion.ObtenerConexion()) {
                 Statement st = cn.createStatement();
                 String sql = "SELECT MAX(codPersona) FROM persona";
-                try (ResultSet rs = st.executeQuery(sql)) {
+                try ( ResultSet rs = st.executeQuery(sql)) {
                     if (rs.next()) {
                         cod = rs.getInt(1);
                         if (rs.wasNull()) {
@@ -50,10 +51,10 @@ public class personaDatos {
     public static List<persona> LeerPersona() throws SQLException {
         List<persona> personas = new ArrayList<>();
         try {
-            try (Connection cn = (Connection) conexion.ObtenerConexion()) {
+            try ( Connection cn = (Connection) conexion.ObtenerConexion()) {
                 Statement st = cn.createStatement();
                 String sql = "SELECT codPersona, nomPersona, ciudadPersona FROM persona";
-                try (ResultSet rs = st.executeQuery(sql)) {
+                try ( ResultSet rs = st.executeQuery(sql)) {
                     while (rs.next()) {
                         persona person = new persona();
                         person.setCodPersona(rs.getInt(1));
@@ -74,9 +75,9 @@ public class personaDatos {
     public static String InsertarPersona(persona person) throws SQLException {
         try {
             int cod = secuenciaCodPersona();
-            try (Connection cn = (Connection) conexion.ObtenerConexion()) {
+            try ( Connection cn = (Connection) conexion.ObtenerConexion()) {
                 String sql = "INSERT INTO persona VALUES(?,?,?)";
-                try (PreparedStatement ps = cn.prepareStatement(sql)) {
+                try ( PreparedStatement ps = cn.prepareStatement(sql)) {
                     ps.setInt(1, cod);
                     ps.setString(2, person.getNomPersona());
                     ps.setString(3, person.getCiudad());
@@ -92,9 +93,9 @@ public class personaDatos {
 
     public static String ActualizaPersona(persona person) throws SQLException {
         try {
-            try (Connection cn = (Connection) conexion.ObtenerConexion()) {
+            try ( Connection cn = (Connection) conexion.ObtenerConexion()) {
                 String sql = "UPDATE persona SET nomPersona= ?, ciudadPersona=? WHERE codPersona=?";
-                try (PreparedStatement ps = cn.prepareStatement(sql)) {
+                try ( PreparedStatement ps = cn.prepareStatement(sql)) {
                     ps.setString(1, person.getNomPersona());
                     ps.setString(2, person.getCiudad());
                     ps.setInt(3, person.getCodPersona());
@@ -107,31 +108,31 @@ public class personaDatos {
         }
         return null;
     }
-    
+
     public static String EliminarPersona(persona person) throws SQLException {
-      try {
-          try (Connection cn = (Connection) conexion.ObtenerConexion()) {
-              String sql = "DELETE FROM persona WHERE codPersona=?";
-              try (PreparedStatement ps = cn.prepareStatement(sql)) {
-                  ps.setInt(1, person.getCodPersona());
-                  ps.execute();
-              }
-          }
+        try {
+            try ( Connection cn = (Connection) conexion.ObtenerConexion()) {
+                String sql = "DELETE FROM persona WHERE codPersona=?";
+                try ( PreparedStatement ps = cn.prepareStatement(sql)) {
+                    ps.setInt(1, person.getCodPersona());
+                    ps.execute();
+                }
+            }
         } catch (SQLException e) {
             throw new SQLException(e.getMessage());
         }
         return null;
     }
-    
-    public static List<persona> BuscarPersona(persona person) throws SQLException, Exception{
+
+    public static List<persona> BuscarPersona(persona person) throws SQLException, Exception {
         List<persona> personas = new ArrayList<>();
         try {
-            try (Connection cn = (Connection) conexion.ObtenerConexion()) {
+            try ( Connection cn = (Connection) conexion.ObtenerConexion()) {
                 Statement st = cn.createStatement();
                 String sql = "SELECT codPersona, nomPersona, ciudadPersona FROM persona WHERE UPPER(nomPersona) LIKE ?";
-                try (PreparedStatement ps = cn.prepareStatement(sql)) {
+                try ( PreparedStatement ps = cn.prepareStatement(sql)) {
                     ps.setString(1, "%" + person.getNomPersona().toUpperCase() + "%");
-                    try (ResultSet rs = ps.executeQuery()) {
+                    try ( ResultSet rs = ps.executeQuery()) {
                         if (rs.next()) {
                             do {
                                 persona personaObjeto = new persona();
@@ -140,10 +141,10 @@ public class personaDatos {
                                 personaObjeto.setCiudad(rs.getString(3));
                                 personas.add(personaObjeto);
                             } while (rs.next());
-                            
+
                         } else {
                             throw new Exception("No encontro coincidencia");
-                            
+
                         }
                     }
                 }
@@ -155,7 +156,5 @@ public class personaDatos {
         }
         return personas;
     }
-
-
 
 }
